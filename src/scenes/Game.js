cfa9e1b94f2d34d56d385da476a6689e967bc6a1
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-07-03 20:14:28 -0500

    Movement and camera finished

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,6 +6,7 @@ export default class Game extends Phaser.Scene
 
     player; //player is now a property of class Game
     platforms; // platform is now a property of class Game
+    cursors; //property to use the keyboard
 
     constructor()
     {
@@ -21,16 +22,19 @@ export default class Game extends Phaser.Scene
 
         //Preloading image for player
         this.load.image("bunny-stand", "assets/Players/bunny1_stand.png");
+
+        //Preloading the controls
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create()
     {
         //Adding the background to the scene
-        this.add.image(240, 320, "background");
+        this.add.image(240, 320, "background").setScrollFactor(1,0);
         //Adding the platform to the scene
         //this.physics.add.staticImage(240, 320, "platform").setScale(0.5);
         
-        //Creating multiple platforms
+        //Creating multiple static platforms
         this.platforms = this.physics.add.staticGroup();
 
         for (let i = 0; i < 5; i++)
@@ -58,15 +62,49 @@ export default class Game extends Phaser.Scene
 
         //Set the camera to follow the player
         this.cameras.main.startFollow(this.player);
+        //Set horizontal deadZones for camera
+        this.cameras.main.setDeadzone(this.scale.width * 1.5);
     }
 
     update() 
     {
-        this.platforms.children.iterate( child => {} )
+        this.platforms.children.iterate( child => {
+            const platform = child;
+            const scrollY = this.cameras.main.scrollY;
+
+            if (platform.y >= scrollY + 700){
+                platform.y = scrollY - Phaser.Math.Between(50, 100);
+                platform.x = Phaser.Math.Between(80, 400);
+                platform.body.updateFromGameObject();
+            }
+        } )
+
         const touchingDown = this.player.body.touching.down;
+
+        //Moving left and Right logic
+        if (this.cursors.left.isDown && !touchingDown){
+            this.player.setVelocityX(-200);
+        } else if (this.cursors.right.isDown && !touchingDown){
+            this.player.setVelocityX(200);
+        } else {
+            this.player.setVelocityX(0);
+        }
 
         if (touchingDown) {
             this.player.setVelocityY(-300);
+        }
+
+        this.horizontalWrap(this.player);
+    }
+
+    horizontalWrap(sprite){
+        const halfWidth = sprite.displayWidth * 0.5;
+        const gameWidth = this.scale.width;
+
+        if (sprite.x < -halfWidth){
+            sprite.x = gameWidth + halfWidth;
+        } else if (sprite.x > gameWidth + halfWidth){
+            sprite.x = -halfWidth;
         }
     }
 }
